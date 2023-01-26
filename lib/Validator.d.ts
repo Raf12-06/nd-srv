d318e88eeeb322289a895061fc3c7776a2188c75
Rafail <myhametshin.rafael@outlook.com>
2023-01-26 22:45:27 +0400

    init

--- lib/Validator.d.ts
+++ lib/Validator.d.ts
@@ -13,7 +13,7 @@ export type Schema = Record<string, SchemaField>
 
 export declare class Validator {
 
-    public static validate(schema: SchemaField, body: Object): void;
+    public static validate(schema: Schema, body: Object): void;
 
     public static checkReference(filed: string, value: any, rule: SchemaField): void;
 }
